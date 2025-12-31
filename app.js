8b80f54133372d1b2067d7b45587ba7a39062e93
Mankhedi Mankge <codanonymous27@gmail.com>
2025-12-31 22:24:21 +0200

    Add first node modules

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
-const http = require('http');
+const _ = require("lodash");
 
-const server = http.createServer((req, res) => {
+const items = [1,[2,[3,[4]]]]
 
-    switch(req.url){
-        case "/": 
-        res.end(`<h1>Welcome to the home page!</h1>`);
-        break;
-
-        case "/about": 
-        res.end(`<h1>Here's some info about us</h1>`);
-        break;
-
-        case "/contact":
-        res.end("<h1>Here's our contact info</h1>");
-        break;
-
-        default: 
-        res.end(`<h1>Oops!</h1>
-                <p>We couldn't locate the page you are looking for</p>
-                <a href = "/">Back to home</a>`);
-    }
-})
-
-server.listen(5000);    
+const newItems = _.flattenDeep(items);
+console.log(newItems);
