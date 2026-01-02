74bd01d9edeedb5ce93de880ce033bc0374a7ef5
Mankhedi Mankge <codanonymous27@gmail.com>
2026-01-02 18:20:20 +0200

    Read data stream (textfile)

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const _ = require("lodash");
+const http = require("http");
+const {createReadStream} = require("fs");
+const { create } = require("lodash");
 
-const items = [1,[2,[3,[5]]]]
+http
+.createServer( (req, res) => {
+      const fileStream = createReadStream("./content/big-file.txt", "utf8");
+      fileStream.on("open", ()=>{
+        fileStream.pipe(res);
+      })
 
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+      fileStream.on("error", (err)=> console.log(err));
+      
+    }
+)
+.listen(5000)
